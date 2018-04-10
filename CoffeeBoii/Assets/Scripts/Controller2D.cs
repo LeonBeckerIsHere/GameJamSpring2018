bef9d75080e8e950acf7f307cb64eac099a79c5a
Leon Becker <lab160730@utdallas.edu>
2018-04-10 11:27:53 -0500

    First Commit

---
+++ CoffeeBoii/Assets/Scripts/Controller2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+    BoxCollider2D boxCollider;
+
+	// Use this for initialization
+	void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
