ee8a8496e524745f449de6bed9207eaae75d6237
Leon Becker <lab160730@utdallas.edu>
2018-04-10 23:54:22 -0500

    Split Controller2D into other scripts to allow for better polymorphism. Got moving platforms working, but Player collisions on top of a moving platform or forced by a moving platform or totally out of whack.

--- CoffeeBoii/Assets/Scripts/Controller2D.cs
+++ CoffeeBoii/Assets/Scripts/Controller2D.cs
@@ -2,27 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
-public class Controller2D : MonoBehaviour {
 
-    public LayerMask collisionMask;
-    public int horizontalRayCount = 4, verticalRayCount = 4;
+public class Controller2D : RaycastController {
 
-    const float skinWidth = 0.015f;
-    float horizontalRaySpacing, verticalRaySpacing;
     float maxClimbAngle = 80;
     float maxDescendAngle = 75;
 
-    BoxCollider2D boxCollider;
-    RaycastOrigins raycastOrigins;
     public CollisionInfo collisions;
 
-
     // Use this for initialization
-    void Start ()
+    public override void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
-        CalculateRaySpacing();
+        base.Start();
     }
 
     public void Move(Vector3 velocity)
@@ -191,34 +182,6 @@ public class Controller2D : MonoBehaviour {
                 }
             }
         }
-    }
-
-    void UpdateRaycastOrigins()
-    {
-        Bounds bounds = boxCollider.bounds;
-        bounds.Expand(skinWidth * -2);
-
-        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-    }
-
-    void CalculateRaySpacing()
-    {
-        Bounds bounds = boxCollider.bounds;
-        bounds.Expand(skinWidth * -2);
-
-        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
-        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
-
-        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
-        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
-    }
-
-    struct RaycastOrigins
-    {
-        public Vector2 topLeft, topRight, bottomLeft, bottomRight;
     }
 
     public struct CollisionInfo
---
+++ CoffeeBoii/Assets/Scripts/RaycastController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RaycastController : MonoBehaviour {
+
+    public LayerMask collisionMask;
+
+    public int horizontalRayCount = 4, verticalRayCount = 4;
+
+    public const float skinWidth = 0.015f;
+
+    [HideInInspector]
+    public float horizontalRaySpacing, verticalRaySpacing;
+
+    [HideInInspector]
+    public BoxCollider2D boxCollider;
+    public RaycastOrigins raycastOrigins;
+
+    public virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    public void UpdateRaycastOrigins()
+    {
+        Bounds bounds = boxCollider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    public void CalculateRaySpacing()
+    {
+        Bounds bounds = boxCollider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    public struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight, bottomLeft, bottomRight;
+    }
+}
